be005fc6a2b7c9317fbb19c267a92c9ad1ad2627
Alex Eliacik <57728302+alexeliacik@users.noreply.github.com>
2020-05-14 01:04:34 -0400

    Update Movies.js

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,38 +3,34 @@ import Pagination from "@material-ui/lab/Pagination"
 
 import StyledContainer, { Row, Card, PaginationDiv } from "./StyledMovies"
 
-const Movies = props => {
+const Movies = (props) => {
   return (
     <StyledContainer>
       <Row>
         {props.movies !== null &&
-          props.movies.map(
-            res =>
-              res.poster_path &&
-              res.backdrop_path && (
-                <Card key={res.id} res={res} showModal={props.showModal}>
-                  <div className='front'>
-                    <h1 className='ratingFront'>{res.vote_average}</h1>
-                  </div>
-                  <div className='back'>
-                    <h1 className='title'>{res.title}</h1>
-                    <h1 className='rating'>{res.vote_average}</h1>
-                    <h3 className='voteCount'>{res.vote_count} VOTE</h3>
-                    <span className='language'>
-                      <i className='fontawasomeIcons fas fa-globe-americas'></i>
-                      {res.original_language}
-                    </span>
-                    <span className='releaseDate'>
-                      <i className='fontawasomeIcons far fa-calendar-alt'></i>
-                      {res.release_date.slice(0, 4)}
-                    </span>
-                    <button className='btn' onClick={() => props.openModal(res)}>
-                      <span className='btnText'>OVERVIEW</span>
-                    </button>
-                  </div>
-                </Card>
-              )
-          )}
+          props.movies.map((res) => (
+            <Card key={res.id} res={res} showModal={props.showModal}>
+              <div className='front'>
+                <h1 className='ratingFront'>{res.vote_average}</h1>
+              </div>
+              <div className='back'>
+                <h1 className='title'>{res.title}</h1>
+                <h1 className='rating'>{res.vote_average}</h1>
+                <h3 className='voteCount'>{res.vote_count} VOTE</h3>
+                <span className='language'>
+                  <i className='fontawasomeIcons fas fa-globe-americas'></i>
+                  {res.original_language}
+                </span>
+                <span className='releaseDate'>
+                  <i className='fontawasomeIcons far fa-calendar-alt'></i>
+                  {res.release_date.slice(0, 4)}
+                </span>
+                <button className='btn' onClick={() => props.openModal(res)}>
+                  <span className='btnText'>OVERVIEW</span>
+                </button>
+              </div>
+            </Card>
+          ))}
 
         <PaginationDiv>
           <Pagination count={props.totalPageCount} page={props.currentPage} shape='rounded' onChange={props.paginationHandler} />
